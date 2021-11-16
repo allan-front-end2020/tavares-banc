26cc09b19b6ea7ba5562b78e707823832406561a
Allan Tavares <allan.tavares@estudante.ufla.br>
2021-11-16 18:36:04 -0300

    first commit

---
+++ banck/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header  text="Tavares-banck e foda!!!" color='#f00'/>     
+    </div>
+  );
+}
+
+export default App;
---
+++ banck/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Header.scss"
+
+
+
+const Header = ({text, color} ) =>(<h1  style={{color}}>{text} </h1>);
+
+
+
+
+export default Header;
